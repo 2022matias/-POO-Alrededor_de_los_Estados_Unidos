33d57c026cca81bbff98e41f9c0b45801eee5722
Matías Agustín Campos <lolo252000@gmail.com>
2022-11-22 00:04:35 -0300

    ultimando detalles

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const openFormButton = document.querySelector('.edit_button');
 
+const popup = document.querySelector('.popup');
+
 const popupContainer = document.querySelector('.popup__container');
 
 const popupCloseIcon = popupContainer.querySelector('.popup__close-icon');
@@ -7,11 +9,15 @@ const popupCloseIcon = popupContainer.querySelector('.popup__close-icon');
 function openForm() {
 	inputName.value = profileName.textContent;
   inputSkill.value = profileSkill.textContent;
-  popupContainer.classList.toggle('popup_visible');
+  popup.classList.remove('popup_visible');
+}
+
+function closeForm () {
+	popup.classList.add('popup_visible');
 }
 
 openFormButton.addEventListener('click', openForm);
-popupCloseIcon.addEventListener('click', openForm); 
+popupCloseIcon.addEventListener('click', closeForm); 
 
 
 
@@ -26,6 +32,7 @@ function handleFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileSkill.textContent = inputSkill.value;
+	popup.classList.add('popup_visible');
 }
 
 popupContainer.addEventListener('submit', handleFormSubmit);
