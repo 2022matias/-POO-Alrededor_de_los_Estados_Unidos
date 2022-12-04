ed0a57bb9fd302b9d4ba8a9882aa1e70a2047cea
Matías Agustín Campos <lolo252000@gmail.com>
2022-12-04 03:52:55 -0300

    empezando el proyecto 5

--- script.js
+++ script.js
@@ -7,18 +7,15 @@ const profileSkill = document.querySelector('.profile__explorer');
 const inputName = document.querySelector('.popup__name');
 const inputSkill = document.querySelector('.popup__skill'); 
 
-function openForm() {
+
+function toggle(){
 	inputName.value = profileName.textContent;
   inputSkill.value = profileSkill.textContent;
-  popup.classList.remove('popup__visible');
+  popup.classList.toggle('popup__visible');
 }
 
-function closeForm () {
-	popup.classList.add('popup__visible');
-}
-
-openFormButton.addEventListener('click', openForm);
-popupCloseIcon.addEventListener('click', closeForm); 
+openFormButton.addEventListener('click', toggle);
+popupCloseIcon.addEventListener('click', toggle); 
 
 
 function handleFormSubmit(event) {
@@ -29,3 +26,89 @@ function handleFormSubmit(event) {
 }
 
 popupContainer.addEventListener('submit', handleFormSubmit);
+
+
+
+
+
+
+
+
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+
+initialCards.array.forEach((link, name)=> {
+  const cardTemplate = document.querySelector("#elements").content;
+  let cardElements = document.querySelector(".elements");
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+  cardElement.querySelector(".element__image").src = link.value;
+  cardElement.querySelector("element__name").textContent = name.value;
+  cardElements.append(cardElement);
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const popupTemplate = document.querySelector(".popup").content;
+const popupCard = document.querySelector(".popup__card");
+const popupContain = document.querySelector('.popup').cloneNode(true);
+
+popupContain.querySelector(".popup__tittle").textContent = "Nuevo lugar";
+popupContain.querySelector(".popup__name").textContent = "Título";
+popupContain.querySelector(".popup__skill").textContent = "Enlace a la imagen";
+
+popupCard.append(popupContain);
+
+
+const openPopupContain = document.querySelector('.profile__add-button');
+openPopupContain.addEventListener('click', toggleCard);
+popupCloseIcon.addEventListener('click', toggleCard); 
+
+function toggleCard(){
+	popupCard.classList.toggle('popup__visible');
+}
