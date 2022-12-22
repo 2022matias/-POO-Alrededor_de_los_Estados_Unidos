013e8437ddcc65b28b1318c40ffb8d961abfb5b8
Matías Agustín Campos <lolo252000@gmail.com>
2022-12-21 23:35:37 -0300

    validacion de 2 input terminado

--- validate.js
+++ validate.js
@@ -1,88 +1,18 @@
-/*
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
-  errorElement.textContent = "";
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  console.log(hasInvalidInput(inputList));
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("button_inactive");
-  } else {
-    buttonElement.classList.remove("button_inactive");
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-  const buttonElement = formElement.querySelector(".form__submit");
-
-  toggleButtonState(inputList, buttonElement);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("entrada", function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form"));
-  formList.forEach((formElement) => {
-    formElement.addEventListener("Enviar", function (evt) {
-      evt.preventDefault();
-    });
-
-    const fieldsetList = Array.from(formElement.querySelectorAll(".form__set"));
-
-    fieldsetList.forEach((fieldset) => {
-      setEventListeners(fieldset);
-    });
-  });
-};
-
-enableValidation();
-*/
-
-const formElement = document.querySelector(".form");
-const formInput = formElement.querySelector(".form__input");
+const formElement = document.querySelector(".popup__form");
+const formInput = formElement.querySelector(".popup__input");
 
 const showInputError = (formElement, inputElement, errorMessage) => {
 	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-	inputElement.classList.add("form__input_type_error");
+	errorElement.classList.add("popup__error_visible");
 	errorElement.textContent = errorMessage;
-	errorElement.classList.add("form__input-error_active");
+	errorElement.classList.add("popup__input_type_error");
 };
 
 
 const hideInputError = (formElement, inputElement) => {
 	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-	inputElement.classList.remove("form__input_type_error");
-	errorElement.classList.remove("form__input-error_active");
+	errorElement.classList.remove("popup__error_visible");
+	errorElement.classList.remove("popup__input_type_error");
 	errorElement.textContent = "";
 };
 
@@ -96,7 +26,7 @@ const isValid = (formElement, inputElement) => {
 };
 
 const setEventListeners = (formElement) => {
-	const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+	const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
 	const buttonElement = formElement.querySelector(".popup__button");
 	inputList.forEach((inputElement) => {
 		inputElement.addEventListener("input", () => {
@@ -107,7 +37,7 @@ const setEventListeners = (formElement) => {
 }; 
 
 const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form"));
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
